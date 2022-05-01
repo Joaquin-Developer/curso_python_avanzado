ebf24e82de642c986a2d9a4913af06e4fe9b533b
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-05-01 20:09:39 -0300

    agrego controlador de valor maximo en iterador

--- fibonacci_iterador.py
+++ fibonacci_iterador.py
@@ -5,6 +5,9 @@ import time
 class FiboIter:
     """representación de los objetos de tipo iterador"""
 
+    def __init__(self, max=None):
+        self.max = max
+
     def __iter__(self):
         self.num1 = 0
         self.num2 = 1
@@ -12,26 +15,27 @@ class FiboIter:
         return self
 
     def __next__(self):
-        if self.counter == 0:
+        if not self.max or self.counter <= self.max:
+            if self.counter == 0:
+                self.counter += 1
+                return self.num1
+            elif self.counter == 1:
+                self.counter += 1
+                return self.num2
+            
+            self.aux = self.num1 + self.num2
+            self.num1, self.num2 = self.num2, self.aux
             self.counter += 1
-            return self.num1
-        elif self.counter == 1:
-            self.counter += 1
-            return self.num2
-        
-        self.aux = self.num1 + self.num2
-        self.num1, self.num2 = self.num2, self.aux
-        self.counter += 1
-        return self.aux
+            return self.aux
 
+        raise StopIteration
 
 def main():
     """main function"""
-    fib = FiboIter()
+    fib = FiboIter(max=8)
     for element in fib:
         print(element)
-        if fib.counter > 10:
-            break
+        time.sleep(0.70)
 
 if __name__ == "__main__":
     main()
